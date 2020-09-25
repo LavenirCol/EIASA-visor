69aafacf988cb8ef4729651cfe98d2a22dc2c17a
Julio Bobadilla <jcbobadi@msn.com>
2020-09-25 00:29:00 -0500

    upload
    upload

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,6 +18,7 @@ class AppAsset extends AssetBundle
         'theme/dashforge/css/dashforge.css',
         'theme/dashforge/css/dashforge.filemgr.css',
         'theme/dashforge/css/skin.charcoal.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/dropzone.min.css',
         'https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.21/b-1.6.3/b-colvis-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/r-2.2.5/sp-1.1.1/datatables.min.css'
     ];
     public $js = [
@@ -29,7 +30,7 @@ class AppAsset extends AssetBundle
         'theme/dashforge/js/dashforge.aside.js',
         'theme/dashforge/js/dashforge.filemgr.js',
         'theme/dashforge/lib/js-cookie/js.cookie.js',
-        //'theme/dashforge/js/dashforge.settings.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/dropzone.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js',
         'https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.21/b-1.6.3/b-colvis-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/r-2.2.5/sp-1.1.1/datatables.min.js',
--- frontend/web/js/filemanager.js
+++ frontend/web/js/filemanager.js
@@ -281,3 +281,71 @@ function addFile(){
         return;
     }
 }
+
+
+//uploadas Handles
+var Upload = function (file, id) {
+    this.file = file;
+    this.id = id;
+};
+
+Upload.prototype.getType = function () {
+    return this.file.type;
+};
+Upload.prototype.getSize = function () {
+    return this.file.size;
+};
+Upload.prototype.getName = function () {
+    return this.file.name;
+};
+Upload.prototype.doUpload = function (inputresult) {
+    var that = this;
+    var formData = new FormData();
+    $("#progress-wrp").show();
+
+    // add assoc key values, this will be posts values
+    formData.append("id", this.id);
+    formData.append("file", this.file, this.getName());
+    formData.append("upload_file", true);
+
+    $.ajax({
+        type: "POST",
+        url: "/webapi/UploadFile",
+        xhr: function () {
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) {
+                myXhr.upload.addEventListener('progress', that.progressHandling, false);
+            }
+            return myXhr;
+        },
+        success: function (result) {
+            // your callback here
+            $(inputresult).val(result.data.archivo);
+            setTimeout(function () {
+                $("#progress-wrp").hide();
+            }, 500);
+        },
+        error: function (error) {
+            // handle error
+        },
+        async: true,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        timeout: 60000
+    });
+};
+
+Upload.prototype.progressHandling = function (event) {
+    var percent = 0;
+    var position = event.loaded || event.position;
+    var total = event.total;
+    var progress_bar_id = "#progress-wrp";
+    if (event.lengthComputable) {
+        percent = Math.ceil(position / total * 100);
+    }
+    // update progressbars classes so it fits your code
+    $(progress_bar_id + " .progress-bar").css("width", +percent + "%");
+    $(progress_bar_id + " .status").text(percent + "%");
+};
