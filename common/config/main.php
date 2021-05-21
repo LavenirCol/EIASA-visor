7a6432b2eabb83aa3dc276e635e22a0a61001cf7
Ricardo Gonzalez Cubillos <77466473+ricardogonzalez-lavenir@users.noreply.github.com>
2021-05-21 07:48:15 -0500

    borrado

--- common/config/main.php
+++ common/config/main.php
@@ -12,8 +12,8 @@ return [
         's3' => [
             'class' => 'bpsys\yii2\aws\s3\Service',
             'credentials' => [
-                'key' => 'AKIA3U2TKEUPSRMPDX67',
-                'secret' => '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',
+                'key' => '',
+                'secret' => '',
             ],
             'region' => 'us-west-2',
             'defaultBucket' => 'umbrella2',
